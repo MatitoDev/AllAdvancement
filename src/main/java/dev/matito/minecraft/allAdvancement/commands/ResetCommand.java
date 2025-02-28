41f9b54fc2b6f84e78cd00726e30f7d779ce8bf1
Matito <77804268+MatitoDev@users.noreply.github.com>
2025-02-28 22:37:23 +0100

    improve reset command

--- src/main/java/dev/matito/minecraft/allAdvancement/commands/ResetCommand.java
+++ src/main/java/dev/matito/minecraft/allAdvancement/commands/ResetCommand.java
@@ -7,22 +7,39 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.event.ClickEvent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class ResetCommand {
     public static void reset(CommandSender sender) {
         sender.sendMessage(AllAdvancement.getPrefix().append(
                 Component.text("Click ", NamedTextColor.YELLOW)
                         .append(Component.text("HERE", NamedTextColor.YELLOW, TextDecoration.BOLD, TextDecoration.UNDERLINED).clickEvent(ClickEvent.callback(audience -> {
+                            try {
+                                FileUtils.cleanDirectory(new File("world/advancements"));
+                                sender.sendMessage(AllAdvancement.getPrefix().append(Component.text("Successfully removed all advancements from Players!", NamedTextColor.YELLOW)));
+                            } catch (IOException e) {
+                                sender.sendMessage(AllAdvancement.getPrefix().append(Component.text("Failed to remove all advancements from Players!")));
+                            }
+
                             int i = AllAdvancement.INSTANCE.getAdvancementsTable().removeAll();
                             if (i <= 0) {
-                                sender.sendMessage(AllAdvancement.getPrefix().append(Component.text("Failed to remove all advancements!")));
+                                sender.sendMessage(AllAdvancement.getPrefix().append(Component.text("Failed to remove all advancements from Database!")));
                                 AllAdvancement.INSTANCE.getHud().update();
                                 return;
                             }
+
                             AllAdvancement.INSTANCE.getHud().update();
-                            sender.sendMessage(AllAdvancement.getPrefix().append(Component.text("Successfully removed " + i + " advancements!", NamedTextColor.YELLOW)));
+                            sender.sendMessage(AllAdvancement.getPrefix().append(Component.text("Successfully removed " + i + " advancements from Database!", NamedTextColor.YELLOW)));
 
                         })))
                         .append(Component.text(" to reset all advancements.", NamedTextColor.YELLOW))));
